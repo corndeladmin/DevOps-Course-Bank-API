10b83ddcc16c3d206bc6c7db8b58fe05afb76982
Robert Owen <robert.owen@softwire.com>
2020-06-04 11:58:59 +0100

    exercise 2 solution

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,8 @@
 from typing import Type
 from flask.testing import FlaskClient
 from flask.wrappers import Response
+import json
+
 import pytest
 
 from bank_api.app import create_app
@@ -15,9 +17,19 @@ def client():
         yield test_client
 
 
-def test_account_creation(client: FlaskClient):
-    # Use the client to make requests to the Flask app.
-    # response = client.get('/example/route')
-    # Or use client.post to make a POST request
-    # https://flask.palletsprojects.com/en/1.1.x/testing/
-    pass
+def test_create_account(client: FlaskClient):
+    """I should be able to create, then query an account"""
+    response_creation = client.post('/accounts/test')
+    response_query = client.get('/accounts/test')
+
+    assert response_creation.status_code == 200
+    assert response_query.status_code == 200
+
+    data = json.loads(response_query.data)
+    assert data['name'] == 'test'
+
+
+def test_get_invalid_account_fails_with_404(client: FlaskClient):
+    """Querying a non-existant account returns 404 (Not Found)"""
+    response_query = client.get('/accounts/nothere')
+    assert response_query.status_code == 404
